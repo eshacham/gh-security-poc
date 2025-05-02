db6bf089050f22502a1f8aee6a655a22097671e6
Eyal Shacham <shacham@swbell.net>
2025-05-02 12:13:53 -0500

    Add SQL injection vulnerability for testing

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-import subprocess
+import sqlite3
 
-def dangerous(user_input):
-    # BAD: directly passes untrusted user_input into a shell
-    subprocess.run(user_input, shell=True)
+def vulnerable_sql(user_input):
+    # BAD: Directly concatenates user input into an SQL query
+    conn = sqlite3.connect(":memory:")
+    conn.execute(f"SELECT * FROM users WHERE name = '{user_input}'")
+    conn.close()
 
 if __name__ == "__main__":
-    user_input = input("Enter something: ")
-    dangerous(user_input)
+    user_input = input("Enter your name: ")
+    vulnerable_sql(user_input)
