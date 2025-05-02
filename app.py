98f0d8fa3d85c3f909ed704f09ead1a3bcd3900d
Eyal Shacham <shacham@swbell.net>
2025-05-02 09:46:59 -0500

    get user input to run as os.system to trip codeql

--- app.py
+++ app.py
@@ -5,4 +5,6 @@ def dangerous(user_input):
     os.system(f"echo You said: {user_input}")
 
 if __name__ == "__main__":
-    dangerous("$(rm -rf /)")  # CodeQL will flag this
+    # now we read from stdin so CodeQL treats this as tainted
+    user_input = input("Enter something: ")
+    dangerous(user_input)  # CodeQL will flag this
